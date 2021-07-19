f8b3d131b6494e5d1b648293400fd6d5e0fe45d5
Lalit Joshi <lalit.joshi@pcdhc.com>
2021-07-19 23:12:24 +0530

    Update index.js

--- LalitJoshi/index.js
+++ LalitJoshi/index.js
@@ -1,1 +1,587 @@
-console.log("nodejs-tutorial");
+const responseData =
+
+    [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "username": "Bret",
+            "email": "Sincere@april.biz",
+            creditAmount: 22424,
+            debitAmount: 2242434,
+            "address": {
+                "street": "Kulas Light",
+                "suite": "Apt. 556",
+                "city": "Gwenborough",
+                "zipcode": "92998-3874",
+                "geo": {
+                    "lat": "-37.3159",
+                    "lng": "81.1496"
+                }
+            },
+            "phone": "1-770-736-8031 x56442",
+            "website": "hildegard.org",
+            "company": {
+                "name": "Romaguera-Crona",
+                "catchPhrase": "Multi-layered client-server neural-net",
+                "bs": "harness real-time e-markets"
+            }
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "username": "Antonette",
+            "email": "Shanna@melissa.tv",
+            creditAmount: 2424,
+            debitAmount: 434,
+            "address": {
+                "street": "Victor Plains",
+                "suite": "Suite 879",
+                "city": "Wisokyburgh",
+                "zipcode": "90566-7771",
+                "geo": {
+                    "lat": "-43.9509",
+                    "lng": "-34.4618"
+                }
+            },
+            "phone": "010-692-6593 x09125",
+            "website": "anastasia.net",
+            "company": {
+                "name": "Deckow-Crist",
+                "catchPhrase": "Proactive didactic contingency",
+                "bs": "synergize scalable supply-chains"
+            }
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "username": "Samantha",
+            "email": "Nathan@yesenia.net",
+            creditAmount: 242434,
+            debitAmount: 4343,
+            "address": {
+                "street": "Douglas Extension",
+                "suite": "Suite 847",
+                "city": "McKenziehaven",
+                "zipcode": "59590-4157",
+                "geo": {
+                    "lat": "-68.6102",
+                    "lng": "-47.0653"
+                }
+            },
+            "phone": "1-463-123-4447",
+            "website": "ramiro.info",
+            "company": {
+                "name": "Romaguera-Jacobson",
+                "catchPhrase": "Face to face bifurcated interface",
+                "bs": "e-enable strategic applications"
+            }
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "username": "Karianne",
+            "email": "Julianne.OConner@kory.org",
+            "address": {
+                "street": "Hoeger Mall",
+                "suite": "Apt. 692",
+                "city": "South Elvis",
+                "zipcode": "53919-4257",
+                "geo": {
+                    "lat": "29.4572",
+                    "lng": "-164.2990"
+                }
+            },
+            "phone": "493-170-9623 x156",
+            "website": "kale.biz",
+            "company": {
+                "name": "Robel-Corkery",
+                "catchPhrase": "Multi-tiered zero tolerance productivity",
+                "bs": "transition cutting-edge web services"
+            }
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "username": "Kamren",
+            "email": "Lucio_Hettinger@annie.ca",
+            "address": {
+                "street": "Skiles Walks",
+                "suite": "Suite 351",
+                "city": "Roscoeview",
+                "zipcode": "33263",
+                "geo": {
+                    "lat": "-31.8129",
+                    "lng": "62.5342"
+                }
+            },
+            "phone": "(254)954-1289",
+            "website": "demarco.info",
+            "company": {
+                "name": "Keebler LLC",
+                "catchPhrase": "User-centric fault-tolerant solution",
+                "bs": "revolutionize end-to-end systems"
+            }
+        },
+        {
+            "id": 6,
+            "name": "Mrs. Dennis Schulist",
+            "username": "Leopoldo_Corkery",
+            "email": "Karley_Dach@jasper.info",
+            "address": {
+                "street": "Norberto Crossing",
+                "suite": "Apt. 950",
+                "city": "South Christy",
+                "zipcode": "23505-1337",
+                "geo": {
+                    "lat": "-71.4197",
+                    "lng": "71.7478"
+                }
+            },
+            "phone": "1-477-935-8478 x6430",
+            "website": "ola.org",
+            "company": {
+                "name": "Considine-Lockman",
+                "catchPhrase": "Synchronised bottom-line interface",
+                "bs": "e-enable innovative applications"
+            }
+        },
+        {
+            "id": 7,
+            "name": "Kurtis Weissnat",
+            "username": "Elwyn.Skiles",
+            "email": "Telly.Hoeger@billy.biz",
+            "address": {
+                "street": "Rex Trail",
+                "suite": "Suite 280",
+                "city": "Howemouth",
+                "zipcode": "58804-1099",
+                "geo": {
+                    "lat": "24.8918",
+                    "lng": "21.8984"
+                }
+            },
+            "phone": "210.067.6132",
+            "website": "elvis.io",
+            "company": {
+                "name": "Johns Group",
+                "catchPhrase": "Configurable multimedia task-force",
+                "bs": "generate enterprise e-tailers"
+            }
+        },
+        {
+            "id": 8,
+            "name": "Nicholas Runolfsdottir V",
+            "username": "Maxime_Nienow",
+            "email": "Sherwood@rosamond.me",
+            creditAmount: 84348,
+            debitAmount: 434334,
+            "address": {
+                "street": "Ellsworth Summit",
+                "suite": "Suite 729",
+                "city": "Aliyaview",
+                "zipcode": "45169",
+                "geo": {
+                    "lat": "-14.3990",
+                    "lng": "-120.7677"
+                }
+            },
+            "phone": "586.493.6943 x140",
+            "website": "jacynthe.com",
+            "company": {
+                "name": "Abernathy Group",
+                "catchPhrase": "Implemented secondary concept",
+                "bs": "e-enable extensible e-tailers"
+            }
+        },
+        {
+            "id": 9,
+            "name": "Glenna Reichert",
+            "username": "Delphine",
+            "email": "Chaim_McDermott@dana.io",
+            "address": {
+                "street": "Dayna Park",
+                "suite": "Suite 449",
+                "city": "Bartholomebury",
+                "zipcode": "76495-3109",
+                "geo": {
+                    "lat": "24.6463",
+                    "lng": "-168.8889"
+                }
+            },
+            "phone": "(775)976-6794 x41206",
+            "website": "conrad.com",
+            "company": {
+                "name": "Yost and Sons",
+                "catchPhrase": "Switchable contextually-based project",
+                "bs": "aggregate real-time technologies"
+            }
+        },
+        {
+            "id": 10,
+            "name": "Clementina DuBuque",
+            "username": "Moriah.Stanton",
+            "email": "Rey.Padberg@karina.biz",
+            "address": {
+                "street": "Kattie Turnpike",
+                "suite": "Suite 198",
+                "city": "Lebsackbury",
+                "zipcode": "31428-2261",
+                "geo": {
+                    "lat": "-38.2386",
+                    "lng": "57.2232"
+                }
+            },
+            "phone": "024-648-3804",
+            "website": "ambrose.net",
+            "company": {
+                "name": "Hoeger LLC",
+                "catchPhrase": "Centralized empowering task-force",
+                "bs": "target end-to-end models"
+            }
+        }
+    ];
+
+
+
+
+console.log(performOperationOnCustomer(8, 'totalBalance', responseData))// should returns a number: 1000 
+// 2. performOperationOnCustomer(undefined, 'allUserTotalBalance', responseData) return [{id:1, totalBalance: 2323},{id:1, totalBalance: 2323}] 
+// 3. performOperationOnCustomer(undefined, 'highestBalance', responseData) 
+// 4. performOperationOnCustomer(undefined, 'lowestBalance', responseData) 
+// 5. performOperationOnCustomer(undefined, 'totalTransactions', responseData) 
+// 6. performOperationOnCustomer(undefined, 'sumAllCreditAmt', responseData) 
+// 7. performOperationOnCustomer(undefined, 'sumAllDebitAmt', responseData) 
+// 8. print user details by id (2, 'userDetail', responseData)
+
+/**
+ * Method to perform operation on customer data
+ * 
+ * @param {*} userId 
+ * @param {*} keyName 
+ * @param {*} responseData 
+ */
+function performOperationOnCustomer(userId, keyName, responseData) {
+    let isValidInput = validateInputs(userId, keyName, responseData)
+    if (isValidInput) {
+        switch (keyName) {
+            case 'totalBalance':
+                return calculateTotalBalance(userId, responseData)
+
+            case 'allUserTotalBalance':
+                return calculateAllUserBalance(responseData)
+
+            case 'highestBalance':
+                return calculateHighestBalance(responseData)
+
+            case 'lowestBalance':
+                return calculateLowestBalance(responseData)
+
+            case 'totalTransactions':
+                return calculateTotalTransactions(responseData)
+
+            case 'sumAllCreditAmt':
+                return calculateSumCreditAmt(responseData)
+
+            case 'sumAllDebitAmt':
+                return calculateSumDebitAmt(responseData)
+
+            case 'userDetail':
+                return getUserDetails(userId, responseData)
+
+        }
+    } else {
+        return 'Invalid Input'
+    }
+}
+
+/**
+ * Method to validate inputs
+ * 
+ * @param {*} userId
+ * @param {*} keyName
+ * @param {*} responseData
+ */
+function validateInputs(userId, keyName, responseData) {
+    const keys = ['allUserTotalBalance', 'highestBalance', 'lowestBalance', 'totalTransactions', 'sumAllCreditAmt', 'sumAllDebitAmt']
+
+    if ((keyName == undefined || keyName == null) && !(keys.includes(keyName) || keyName === "totalBalance" || keyName == "userDetail")) {
+        return false
+    }
+
+    if (isNaN(userId) && !keys.includes(keyName)) {
+        return false
+    }
+
+    if (!Array.isArray(responseData)) {
+        return false
+    }
+
+    return true
+}
+
+/**
+ * Method to return user details of user id provided from response data
+ * 
+ * @param {*} userId 
+ * @param {*} responseData 
+ */
+function getUserDetails(userId, responseData) {
+    responseData.forEach(item => {
+        if (item == undefined) {
+            return 'Invalid Input'
+        } else {
+            if (item["id"] != undefined && item["id"] != null && userId == item["id"]) {
+                return item
+            }
+        }
+    })
+
+    return 'No data found for user Id'
+}
+
+/**
+ * Calculate Total transactions
+ * 
+ * @param {*} responseData 
+ * @returns Number
+ */
+function calculateTotalTransactions(responseData) {
+    let total = 0
+    responseData.forEach(item => {
+        if (item == undefined || item == null) {
+            return 'Invalid Input'
+        } else {
+            if (item["id"] != undefined && item["id"] != null && userId == item["id"]) {
+
+                if (item["creditAmount"] != undefined && item["creditAmount"] != null && !isNaN(item["creditAmount"])) {
+                    total = total + 1
+                }
+
+                if (item["debitAmount"] != undefined && item["debitAmount"] != null && !isNaN(item["debitAmount"])) {
+                    total = total + 1
+                }
+            }
+        }
+    })
+
+    return total
+}
+
+/**
+ * Sum total credit amount
+ * 
+ * @param {*} responseData
+ * @returns Number 
+ */
+function calculateSumCreditAmt(responseData) {
+    let total = 0
+    responseData.forEach(item => {
+        if (item == undefined || item == null) {
+            return 'Invalid Input'
+        } else {
+            if (item["id"] != undefined && item["id"] != null && userId == item["id"]) {
+                let creditAmount = 0
+                let debitAmount = 0
+                if (item["creditAmount"] != undefined && item["creditAmount"] != null && !isNaN(item["creditAmount"])) {
+                    total = total + Number(item["creditAmount"])
+                }
+            }
+        }
+    })
+
+    return total
+}
+
+/**
+ * Sum total debit amount
+ * 
+ * @param {*} responseData
+ * @returns Number 
+ */
+function calculateSumDebitAmt(responseData) {
+    let total = 0
+    responseData.forEach(item => {
+        if (item == undefined || item == null) {
+            return 'Invalid Input'
+        } else {
+            if (item["id"] != undefined && item["id"] != null && userId == item["id"]) {
+                let creditAmount = 0
+                let debitAmount = 0
+                if (item["debitAmount"] != undefined && item["debitAmount"] != null && !isNaN(item["debitAmount"])) {
+                    total = total + Number(item["debitAmount"])
+                }
+            }
+        }
+    })
+
+    return total
+}
+
+/**
+ * Calculate lowest balance
+ * 
+ * @param {*} responseData 
+ * @returns Number
+ */
+function calculateLowestBalance(responseData) {
+    let lowestBalance = 0
+    for (let index = 0; index < responseData.length; index++) {
+        let item = responseData[index]
+
+        if (item == undefined || item == null) {
+            return 'Invalid Input'
+        } else {
+            if (item["id"] != undefined && item["id"] != null) {
+                let creditAmount = 0
+                let debitAmount = 0
+                let total = 0
+                if (item["creditAmount"] != undefined && item["creditAmount"] != null && !isNaN(item["creditAmount"])) {
+                    creditAmount = Number(item["creditAmount"])
+                }
+
+                if (item["debitAmount"] != undefined && item["debitAmount"] != null && !isNaN(item["debitAmount"])) {
+                    debitAmount = Number(item["debitAmount"])
+                }
+
+                total = creditAmount - debitAmount
+
+                if (total < lowestBalance) {
+                    lowestBalance = total
+                }
+                break
+            }
+        }
+    }
+    responseData.forEach(item => {
+        if (item == undefined || item == null) {
+            return 'Invalid Input'
+        } else {
+            if (item["id"] != undefined && item["id"] != null) {
+                let creditAmount = 0
+                let debitAmount = 0
+                let total = 0
+                if (item["creditAmount"] != undefined && item["creditAmount"] != null && !isNaN(item["creditAmount"])) {
+                    creditAmount = Number(item["creditAmount"])
+                }
+
+                if (item["debitAmount"] != undefined && item["debitAmount"] != null && !isNaN(item["debitAmount"])) {
+                    debitAmount = Number(item["debitAmount"])
+                }
+
+                total = creditAmount - debitAmount
+
+                if (total < lowestBalance) {
+                    lowestBalance = total
+                }
+            }
+        }
+    })
+
+    return lowestBalance
+}
+
+/**
+ * Calculate highest balance
+ * 
+ * @param {*} responseData 
+ * @returns Number
+ */
+function calculateHighestBalance(responseData) {
+    let highestBalance = 0
+    responseData.forEach(item => {
+        if (item == undefined || item == null) {
+            return 'Invalid Input'
+        } else {
+            if (item["id"] != undefined && item["id"] != null) {
+                let creditAmount = 0
+                let debitAmount = 0
+                let total = 0
+                if (item["creditAmount"] != undefined && item["creditAmount"] != null && !isNaN(item["creditAmount"])) {
+                    creditAmount = Number(item["creditAmount"])
+                }
+
+                if (item["debitAmount"] != undefined && item["debitAmount"] != null && !isNaN(item["debitAmount"])) {
+                    debitAmount = Number(item["debitAmount"])
+                }
+
+                total = creditAmount - debitAmount
+
+                if (total > highestBalance) {
+                    highestBalance = total
+                }
+            }
+        }
+    })
+
+    return highestBalance
+}
+
+/**
+ * Calculate all user balance
+ * 
+ * @param {*} responseData 
+ * @returns array
+ */
+function calculateAllUserBalance(responseData) {
+    let allUserBalance = []
+    responseData.forEach(item => {
+        if (item == undefined || item == null) {
+            return 'Invalid Input'
+        } else {
+            if (item["id"] != undefined && item["id"] != null) {
+                let creditAmount = 0
+                let debitAmount = 0
+                let total = 0
+                if (item["creditAmount"] != undefined && item["creditAmount"] != null && !isNaN(item["creditAmount"])) {
+                    creditAmount = Number(item["creditAmount"])
+                }
+
+                if (item["debitAmount"] != undefined && item["debitAmount"] != null && !isNaN(item["debitAmount"])) {
+                    debitAmount = Number(item["debitAmount"])
+                }
+
+                total = creditAmount - debitAmount
+                allUserBalance.push({
+                    "id": item["id"],
+                    "totalBalance": total
+                })
+            }
+        }
+    })
+
+    return allUserBalance
+}
+
+/**
+ * Calculate total balance of user
+ * 
+ * @param {*} userId 
+ * @param {*} responseData 
+ * @returns Number
+ */
+function calculateTotalBalance(userId, responseData) {
+    let total = 0
+    responseData.forEach(item => {
+        if (item == undefined || item == null) {
+            return 'Invalid Input'
+        } else {
+            console.log("userId 3==" + userId)
+            if (item["id"] != undefined && item["id"] != null && userId == item["id"]) {
+                let creditAmount = 0
+                let debitAmount = 0
+                if (item["creditAmount"] != undefined && item["creditAmount"] != null && !isNaN(item["creditAmount"])) {
+                    creditAmount = Number(item["creditAmount"])
+                }
+
+                if (item["debitAmount"] != undefined && item["debitAmount"] != null && !isNaN(item["debitAmount"])) {
+                    debitAmount = Number(item["debitAmount"])
+                }
+
+                total = creditAmount - debitAmount
+            }
+        }
+    })
+
+    return total
+}
+
